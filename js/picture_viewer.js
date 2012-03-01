17be7e1c21b35ec8f8608a55ed7670fe5f3950ac
jenniferseal <jenniferlseal@gmail.com>
2012-03-01 10:12:55 -0500

    test comment summery
    some changes

---
+++ js/picture_viewer.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+	$("a.thumbs").fancybox();
+	
+	});
+	
+	
+	
+	/*  HTML
+	<a href="#" class="thumbs"> ... </a>
+	
+	a.thumbs {
+		border: 1px solid #000000;
+		} 
+		
+	*/
+	
